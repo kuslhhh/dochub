0d28f27e2aaa95df62b952b8748d437b3dbf6e6f
kuslhhh <kushaljadhav7745@gmail.com>
2024-09-07 22:02:02 +0530

    sparkles

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -7,21 +7,20 @@ import { useConvexAuth } from "convex/react";
 import { ArrowBigRight, ArrowRight } from "lucide-react";
 import Link from "next/link";
 
-export const Heading = () =>{
-    const { isAuthenticated, isLoading} = useConvexAuth();
-    return(
+export const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+    return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-4xl sm:text-6xl md:text-7xl font-bold italic underline leading-tight text-black dark:text-white mb-4">
-    Introducing <span className=" italic decoration-4 decoration-grey-500">docHub</span>
-</h1>
-<h3 className="text-lg sm:text-2xl md:text-3xl font-medium text-black dark:text-white leading-relaxed">
-    DocHub is the connected workspace for all your ideas, <br />
-    documents, and plans.
-</h3>
+                Introducing <span className=" italic decoration-4 decoration-grey-500">docHub</span>
+            </h1>
+            <h3 className="text-lg sm:text-2xl md:text-3xl font-medium text-black dark:text-white leading-relaxed">
+                let your documents be <span className="italic">accessible</span> and <span className="italic">discoverable</span>
+            </h3>
 
             {isLoading && (
                 <div className="w-full flex items-center justify-center">
-                    <Spinner size="lg"/>
+                    <Spinner size="lg" />
                 </div>
             )}
             {isAuthenticated && !isLoading && (
