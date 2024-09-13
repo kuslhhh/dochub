ee8d9d2cc29ae4fd07052bb56f823aedf977ee5d
kuslhhh <kushaljadhav7745@gmail.com>
2024-09-13 09:43:06 +0530

    button

--- app/(marketing)/_components/footer2.tsx
+++ app/(marketing)/_components/footer2.tsx
@@ -5,14 +5,14 @@ export const Footer2 = () => {
         <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
             <div className="md:ml-auto w-full justify-center md:justify-center flex items-center gap-x-2 text-muted-foreground">
                 <Button variant="ghost" size="sm">
-                    developed by
+                    
                     <a
                         href="https://dochub-livid.vercel.app/"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="font-bold ml-1"
                     >
-                        Kushal
+                       developed by Kushal
                     </a>
                 </Button>
             </div>
